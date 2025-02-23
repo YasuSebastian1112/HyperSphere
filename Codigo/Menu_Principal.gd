e41d9f6b1537480b78754ca2fa5db4b659cf41ac
YasuSebastian1112 <teamyatagarasuoficial@gmail.com>
2025-02-22 22:04:38 -0600

    Escenas relacionadas al menu y la interfaz

---
+++ Codigo/Menu_Principal.gd
@@ -0,0 +1,41 @@
+extends Control
+
+"""Propiedades"""
+
+@onready var iniciar : Label = $Iniciar
+@onready var botones : VBoxContainer = $Botones
+@onready var jugar : Button = $Botones/Jugar
+@onready var configuracion : Button = $Botones/Configuracion
+@onready var extras : Button = $Botones/Extras
+@onready var salir : Button = $Botones/Salir
+
+var iniciado : bool = false
+var en_grab : bool = false
+
+"""Metodos"""
+
+func _input(event) -> void:
+	if event.is_pressed() and iniciado == false:
+		iniciado = true
+		iniciar.hide()
+		botones.show()
+		swichear_botones(false)
+	elif Input.is_action_just_pressed("ui_cancel") and iniciado == true:
+		iniciado = false
+		iniciar.show()
+		botones.hide()
+		en_grab = false
+		swichear_botones(true)
+	if Input.is_action_just_pressed("ui_up") and en_grab == false or Input.is_action_just_pressed("ui_down") and en_grab == false:
+		en_grab = true
+		jugar.grab_focus()
+
+func swichear_botones(valor : bool) -> void:
+	jugar.disabled = valor
+	configuracion.disabled = valor
+	extras.disabled = valor
+	salir.disabled = valor
+
+func _on_jugar_pressed() -> void:
+	print("SsO")
+	pass # Replace with function body.
